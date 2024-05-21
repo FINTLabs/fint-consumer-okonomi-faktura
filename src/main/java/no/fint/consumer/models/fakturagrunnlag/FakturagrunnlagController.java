3d9e22f0e0e62000362fd61f31b4099ed1f6af3b
Hknots <hknots99@gmail.com>
2024-05-21 13:01:17 +0200

    more

--- src/main/java/no/fint/consumer/models/fakturagrunnlag/FakturagrunnlagController.java
+++ src/main/java/no/fint/consumer/models/fakturagrunnlag/FakturagrunnlagController.java
@@ -182,7 +182,7 @@ public class FakturagrunnlagController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(fakturagrunnlag);
+            return linker.mapAndResetLinks(fakturagrunnlag);
         }    
     }
 
--- src/main/java/no/fint/consumer/models/fakturautsteder/FakturautstederController.java
+++ src/main/java/no/fint/consumer/models/fakturautsteder/FakturautstederController.java
@@ -182,7 +182,7 @@ public class FakturautstederController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(fakturautsteder);
+            return linker.mapAndResetLinks(fakturautsteder);
         }    
     }
 
