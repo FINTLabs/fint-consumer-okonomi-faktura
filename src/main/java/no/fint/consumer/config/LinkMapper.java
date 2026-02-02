3a54202add6518aadbf1a8cbf1fa85f4aea03a60
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-02-02 14:38:34 +0100

    Version 4.0.10.

--- src/main/java/no/fint/consumer/config/LinkMapper.java
+++ src/main/java/no/fint/consumer/config/LinkMapper.java
@@ -14,10 +14,10 @@ public class LinkMapper {
             .put(Faktura.class.getName(), contextPath + RestEndpoints.FAKTURA)
             .put(Fakturagrunnlag.class.getName(), contextPath + RestEndpoints.FAKTURAGRUNNLAG)
             .put(Fakturautsteder.class.getName(), contextPath + RestEndpoints.FAKTURAUTSTEDER)
-            .put("no.novari.fint.model.felles.kodeverk.iso.Landkode", "/felles/kodeverk/iso/landkode")
-            .put("no.novari.fint.model.okonomi.kodeverk.Vare", "/okonomi/kodeverk/vare")
-            .put("no.novari.fint.model.felles.Person", "/felles/person")
-            .put("no.novari.fint.model.administrasjon.organisasjon.Organisasjonselement", "/administrasjon/organisasjon/organisasjonselement")
+            .put("no.novari.fint.model.felles.kodeverk.iso.Landkode", "/model/felles/kodeverk/iso/landkode")
+            .put("no.novari.fint.model.okonomi.kodeverk.Vare", "/model/okonomi/kodeverk/vare")
+            .put("no.novari.fint.model.felles.Person", "/model/felles/person")
+            .put("no.novari.fint.model.administrasjon.organisasjon.Organisasjonselement", "/model/administrasjon/organisasjon/organisasjonselement")
             /* .put(TODO,TODO) */
             .build();
     }
